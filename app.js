b75c424b0f7e039e7edc60773a5fbb28a59466b6
Darkzero7 <chaiyanituporn@gmail.com>
2024-05-27 07:02:39 +0700

    js lab 84

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-// write code here
+let text1 = "This is a sample text containing the word XXX.";
+// console.log(containsSensitiveWords(text1)); //true
+
+let text2 = "This is a sample text without any sensitive words.";
+// console.log(containsSensitiveWords(text2)); //false
+
+function containsSensitiveWords(text) {
+  let lowwerText = text.toLowerCase();
+  let findText = ['xxx', 'sex', 'porn'];
+  for (word of findText) {
+    if (lowwerText.includes(word)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+console.log(containsSensitiveWords(text1))
+console.log(containsSensitiveWords(text2))
